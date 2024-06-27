216ae8cf367f779888a54d5a25b108e0ad9de163
Bertoxic <dirusalbert@gmail.com>
2024-06-27 16:02:45 -0700

    "added port in auth to env"

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	config "github.com/bertoxic/med/services/authentication/configs"
@@ -12,36 +15,36 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
-
-
 func main() {
-    err := godotenv.Load("../../../.env")
-    if err != nil {
-        log.Println("Error loading .env file")
-    }
-    app, err := config.NewAppConfig()
-    if err != nil {
-        log.Println("error occured while setting env",err)
-    }
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second *20)
-    defer cancel()
-    client := database.NewMongodClient(ctx,app.Config.MONGODB_URI)
-    app.InProduction = false
-    app.Client = client
-    NewDataB(app)
-    grpcListen()
-    routes := routes.Router()
+	err := godotenv.Load("../../../.env")
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+	app, err := config.NewAppConfig()
+	if err != nil {
+		log.Println("error occured while setting env", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+	client := database.NewMongodClient(ctx, app.Config.MONGODB_URI)
+	app.InProduction = false
+	app.Client = client
+	NewDataB(app)
+	grpcListen()
+	routes := routes.Router()
+	port := os.Getenv("PORT")
+	portnum, err := strconv.Atoi(port)
+	if err != nil {
+		log.Println("error getting or converting port")
+	}
 	srv := &http.Server{
-        Addr: "0.0.0.0:8085",
-        Handler: routes,
-    }
+		Addr:    ":" + fmt.Sprintf("%d", portnum),
+		Handler: routes,
+	}
 
-
-
-    err = srv.ListenAndServe()
-    if err != nil {
-        log.Println("Eroooxxx "+err.Error())
-        return
-    }
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Println("Eroooxxx " + err.Error())
+		return
+	}
 }
